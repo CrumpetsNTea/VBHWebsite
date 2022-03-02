7f1dd7576c3ea71b064ceec43ee782ee5abe4bad
CrumpetsNTea <t.botell@gmail.com>
2022-03-02 18:34:26 -0500

    Add linkedin redirect onclick on team image

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -15,30 +15,38 @@ const Team = () => {
           :{" "}
         </p>
       </div>
-      <div id="team-photos">
-        <div className="container">
-          <img src={Iouri} alt="Iouri" id="iouri"></img>
-          <div className="overlay"></div>
-          <p className="text">
-            Iuori Volkov <br></br>Co-Founder | CEO
-            <br></br>Full-Stack Dev
-          </p>
-        </div>
-        <div className="container">
-          <img src={Tobias} alt="Tobias" id="tobias"></img>
-          <p className="text">
-            Tobias Botell <br></br>Co-Founder | CMO
-            <br></br>Full-Stack Dev
-          </p>
-        </div>
 
-        <div className="container">
-          <img src={Connor} alt="Connor" id="connor"></img>
-          <p className="text">
-            Connor Hughes <br></br>Co-Founder | CTO
-            <br></br>Full-Stack Dev
-          </p>
-        </div>
+      <div id="team-photos">
+        <a href="https://www.linkedin.com/in/iourivolkov/">
+          <div className="container">
+            <img src={Iouri} alt="Iouri" id="iouri"></img>
+            <div className="overlay"></div>
+            <p className="text">
+              Iuori Volkov <br></br>Co-Founder | CEO
+              <br></br>Full-Stack Dev
+            </p>
+          </div>
+        </a>
+
+        <a href="https://www.linkedin.com/in/tobiasbotell/">
+          <div className="container">
+            <img src={Tobias} alt="Tobias" id="tobias"></img>
+            <p className="text">
+              Tobias Botell <br></br>Co-Founder | CMO
+              <br></br>Full-Stack Dev
+            </p>
+          </div>
+        </a>
+
+        <a href="https://www.linkedin.com/in/connor-hughes-90970222b/">
+          <div className="container">
+            <img src={Connor} alt="Connor" id="connor"></img>
+            <p className="text">
+              Connor Hughes <br></br>Co-Founder | CTO
+              <br></br>Full-Stack Dev
+            </p>
+          </div>
+        </a>
       </div>
     </>
   );
